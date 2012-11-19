46f0f435303f33e386365a94d1fed6e203dcafe1
Eric Saxby <sax@livinginthepast.org>
2012-11-18 22:50:00 -0800

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "ops@wanelo.com"
+maintainer_email "Wanelo, Inc."
+license          "Apache 2.0"
+description      "Installs/Configures redis"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+supports "smartos"
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: redis
+# Recipe:: default
+#
+# Copyright 2012, Wanelo
+#
+# Apache 2.0
+#
